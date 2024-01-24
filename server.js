0389c0b136fcf495c23f0b40a30ad3b02c90bdf6
Le-Yo <deoleyo@gmail.com>
2024-01-24 11:19:36 -0600

    nodemailer

--- server.js
+++ server.js
@@ -5,23 +5,12 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require('cors');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const allowedOrigins = ['http://localhost:3000', ];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       return callback(null, true);
-//     }
-
-//     const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-//     return callback(new Error(msg), false);
-//   },
-//   credentials: true,
-// }));
 
  app.use(cors({
     origin: true,
@@ -38,6 +27,7 @@ const connectDB = async () => {
   await mongoose.connect(DB, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    
   });
   console.log("Mongodb is connected");
 };
@@ -50,11 +40,37 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+const transporter = nodemailer.createTransport({
+ 
+  service: 'gmail',
+  auth: {
+    user: 'subcontractracker@gmail.com',
+    pass: 'fved inwq xqxq hnhh'
+  }
+});
+
+    
 app.post('/submit-form', async (req, res) => {
   try {
     const formData = req.body;
 
     console.log('Received form data:', formData);
+    
+    const mailOptions ={
+
+      from: 'subcontractracker@gmail.com',
+      to:   'cowalters@gmail.com',
+      subject: 'New Form Submission',
+      text: JSON.stringify(formData)
+    }
+    
+    try{
+      await transporter.sendMail(mailOptions);
+    }catch(emailError){
+      console.error('error sending the form', emailError)
+      res.status(500).json({error:'Error sending email'});
+    }
+   
 
     const formEntry = new FormModel(formData);
     await formEntry.save();
